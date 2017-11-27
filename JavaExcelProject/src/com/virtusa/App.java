a015268ad51d346904dc38ee9abe54cc00ad0ff3
DELL-PC\DELL <bnithin205@gmail.com>
2017-11-27 23:22:46 +0530

    added new classes

---
+++ JavaExcelProject/src/com/virtusa/App.java
@@ -0,0 +1,92 @@
+package com.virtusa;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class App {
+
+	public static void main(String[] args) {
+		File f = null;
+		InputStream is = null;
+		try {
+			f = new File("D://java/Auto.xls");
+			is = new FileInputStream(f);
+			HSSFWorkbook workbook = new HSSFWorkbook(is);
+			HSSFSheet sheet = workbook.getSheetAt(0);
+			int lastRowNum = sheet.getLastRowNum();
+			ArrayList<Auto> autoList = new ArrayList<Auto>();
+			for (int currentRow = 1; currentRow < lastRowNum; currentRow++) {
+				HSSFRow row = sheet.getRow(currentRow);
+				int lastCellNum = row.getLastCellNum();
+
+				Auto a = new Auto();
+				for(int i=0; i<lastCellNum; i++ ) {
+					HSSFCell cell = row.getCell(i);
+					String value = cell.toString();
+					switch(i) {
+						case 0: a.setMpg(value);break;
+						case 1: a.setCylinders(value);break;
+						case 2: a.setDisplacement(value);break;
+						case 3: a.setHorsepower(value);break;
+						case 4: a.setWeight(value);break;
+						case 5: a.setAcceleration(value);break;
+						case 6: a.setYear(value);break;
+						case 7: a.setOrigin(value);break;
+						case 8: a.setName(value);break;
+					}
+				}
+
+				autoList.add(a);
+			}
+
+			System.out.println("Total : "+autoList.size());
+			
+			Iterator<Auto> iterator = autoList.iterator();
+			ArrayList<Auto> filteredList = new ArrayList<Auto>();
+			while (iterator.hasNext()) {
+				Auto auto = (Auto) iterator.next();
+				boolean isMatchFound = false;
+				if("24.0".equalsIgnoreCase(auto.getMpg())) {
+					if("4.0".equalsIgnoreCase(auto.getCylinders())) {
+						if("113.0".equalsIgnoreCase(auto.getDisplacement())) {
+							//filteredList.add(auto);
+							isMatchFound = true;
+							System.out.println("Hi, match found");
+						}
+					}
+				}
+				if(!isMatchFound) {
+					iterator.remove();
+				}
+			}
+
+			System.out.println("Filtered : "+autoList.size());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();	
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(is!=null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
+
---
+++ JavaExcelProject/src/com/virtusa/Auto.java
@@ -0,0 +1,73 @@
+package com.virtusa;
+
+public class Auto {
+
+		private String  mpg;
+		private String  cylinders;
+		private String  displacement;
+		private String  horsepower;
+		private String  weight;
+		private String  acceleration;
+		private String  year;
+		private String  origin;
+		private String  name;
+		
+		
+		public String getMpg() {
+			return mpg;
+		}
+		public void setMpg(String mpg) {
+			this.mpg = mpg;
+		}
+		public String getCylinders() {
+			return cylinders;
+		}
+		public void setCylinders(String cylinders) {
+			this.cylinders = cylinders;
+		}
+		public String getDisplacement() {
+			return displacement;
+		}
+		public void setDisplacement(String displacement) {
+			this.displacement = displacement;
+		}
+		public String getHorsepower() {
+			return horsepower;
+		}
+		public void setHorsepower(String horsepower) {
+			this.horsepower = horsepower;
+		}
+		public String getWeight() {
+			return weight;
+		}
+		public void setWeight(String weight) {
+			this.weight = weight;
+		}
+		public String getAcceleration() {
+			return acceleration;
+		}
+		public void setAcceleration(String acceleration) {
+			this.acceleration = acceleration;
+		}
+		public String getYear() {
+			return year;
+		}
+		public void setYear(String year) {
+			this.year = year;
+		}
+		public String getOrigin() {
+			return origin;
+		}
+		public void setOrigin(String origin) {
+			this.origin = origin;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+
+}
+
